446c2d71f130575a343d267c4b2e8102fa7660d3
Crowdbotics <team@crowdbotics.com>
2020-06-25 06:01:41 +0000

    Update models

--- backend/productivityx_18412/wsgi.py
+++ backend/productivityx_18412/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'productivityx_18412.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "productivityx_18412.settings")
 
 application = get_wsgi_application()
