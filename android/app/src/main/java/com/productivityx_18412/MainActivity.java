e25c25a926528e8824af7524f51583d180781ae0
Crowdbotics <team@crowdbotics.com>
2020-06-25 06:00:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/productivityx_18412/MainActivity.java
@@ -0,0 +1,15 @@
+package com.productivityx_18412;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "productivityx_18412";
+    }
+}
